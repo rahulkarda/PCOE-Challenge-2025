5eb57a8bb54e5a865607620688905fd637b5d5f7
Rahul Karda <rahul.karda@sap.com>
2025-04-18 10:41:31 +0530

    Removed React StrictMode to prevent double renders

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,9 +5,9 @@ import App from "./App"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
 
 console.log("Frontend server started on http://localhost:3000")
